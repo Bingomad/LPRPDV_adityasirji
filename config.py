10a34c1293a1682eb52a9496a22bc3ad729d963d
Bingomad <125842340+Bingomad@users.noreply.github.com>
2023-12-06 19:33:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6818261867:AAFhXboqTtBzh7vl1Mh05B9z0VZZGHsC9iU")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6642680678:AAE0yAPFD-Rmq284CZhGshjdjgqvXZs6r_A")
     API_ID = int(os.environ["API_ID", 27060147]
     API_HASH = os.environ["API_HASH", "70f93c98c61a40f69c0f830bd4fece7c"]
-    AUTH_USERS = "6693536228 """
+    AUTH_USERS = "5489477491 """
 
